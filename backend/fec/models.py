7abe8be7add8be0505fa754a0f0b2dd72416e0f5
digiwhale <digiwhale.humphrey@gmail.com>
2020-12-04 10:49:02 -0500

    Deleted sqlite corupt database. Re-migrated.

--- backend/fec/models.py
+++ backend/fec/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Politician(models.Model):
-    name = models.CharField(max_length=200, default="XXXXX XXXXXX", unique=True)
+    name = models.CharField(max_length=200, default="XXXXX XXXXXX")
 
     def __str__(self):
         return self.name
