f57aaa83e80e7f39b9b1044be63c76e4be61af4d
digiwhale <digiwhale.humphrey@gmail.com>
2020-12-04 12:52:30 -0500

    cleaned views and added inline css to index.html.

--- backend/fec/views.py
+++ backend/fec/views.py
@@ -1,27 +1,48 @@
 import requests
 from django.shortcuts import render
-from django.views.generic import ListView
 from .models import Politician
-from rest_framework import viewsets
-from .serializer import PoliticianSerializer
-from .models import Politician
-from django.conf import settings
-from .forms import SubmitEmbed
 from django.core.exceptions import ObjectDoesNotExist
-
-
-class PoliticianViewSet(viewsets.ModelViewSet):
-    queryset = Politician.objects.all().order_by('name')
-    serializer_class = PoliticianSerializer
-
-class PoliticianListView(ListView):
-    model = Politician
-    template_name = 'fecapp/index.html'
 
 def politician_list(request):
     politicians = Politician.objects.all()
     return render(request, 'fecapp/index.html', {'x': politicians})
 
+def get_total_page_numbers():
+    response = requests.get("https://api.open.fec.gov/v1/candidates/?api_key=2c0rL4Z709iNErb0gLygJu3UhNjSi7VGPdIWoe1K&page=1&sort=name&per_page=100&sort_nulls_last=false&sort_null_only=false&sort_hide_null=false")
+    data = response.json()
+    pages = data["pagination"]["pages"]
+    return pages
+
+def get_names(request):
+    all_names = {}
+    for i in range(1, get_total_page_numbers()): 
+        response = requests.get(f"https://api.open.fec.gov/v1/candidates/?api_key=2c0rL4Z709iNErb0gLygJu3UhNjSi7VGPdIWoe1K&page={i}&sort=name&per_page=100&sort_nulls_last=false&sort_null_only=false&sort_hide_null=false")
+        data = response.json()
+        names = data['results']
+        for i in range(len(names)):
+            try: 
+                Politician.objects.get(name = names[i]['name'])
+                pass
+            except ObjectDoesNotExist:
+                name_data = Politician(name = names[i]['name'])
+                name_data.save()
+        all_names = Politician.objects.all()
+        print(all_names)
+
+    return render (request, 'fecapp/index.html', { "form": 
+    all_names} )
+
+
+
+
+
+######################### CODE SCRAPS ####################################
+# from django.views.generic import ListView
+# from rest_framework import viewsets
+# from .serializer import PoliticianSerializer
+# from .models import Politician
+# from django.conf import settings
+# from .forms import SubmitEmbed
 # def save_embed(request):
 #     name_database={}
 #     if True:
@@ -49,28 +70,6 @@ def politician_list(request):
 #     print(serializer.errors)
 #     return render(request, 'fecapp/index.html', {'form': name_database})
 
-def get_total_page_numbers():
-    response = requests.get("https://api.open.fec.gov/v1/candidates/?api_key=2c0rL4Z709iNErb0gLygJu3UhNjSi7VGPdIWoe1K&page=1&sort=name&per_page=100&sort_nulls_last=false&sort_null_only=false&sort_hide_null=false")
-    data = response.json()
-    pages = data["pagination"]["pages"]
-    return pages
-
-def get_names(request):
-    all_names = {}
-    for i in range(1, get_total_page_numbers()): 
-        response = requests.get(f"https://api.open.fec.gov/v1/candidates/?api_key=2c0rL4Z709iNErb0gLygJu3UhNjSi7VGPdIWoe1K&page={i}&sort=name&per_page=100&sort_nulls_last=false&sort_null_only=false&sort_hide_null=false")
-        data = response.json()
-        names = data['results']
-        for i in range(len(names)):
-            try: 
-                Politician.objects.get(name = names[i]['name'])
-                pass
-            except ObjectDoesNotExist:
-                name_data = Politician(name = names[i]['name'])
-                name_data.save()
-        all_names = Politician.objects.all()
-        print(all_names)
-
     #     for i in meals:
     #         meal_data = Meal(
     #             name = i['strMeal'],
@@ -83,5 +82,10 @@ def get_names(request):
     #         meal_data.save()
     #         all_meals = Meal.objects.all().order_by('-id')
 
-    return render (request, 'fecapp/index.html', { "form": 
-    all_names} )
+# class PoliticianViewSet(viewsets.ModelViewSet):
+#     queryset = Politician.objects.all().order_by('name')
+#     serializer_class = PoliticianSerializer
+
+# class PoliticianListView(ListView):
+#     model = Politician
+#     template_name = 'fecapp/index.html'
--- frontend/js/App.js
+++ frontend/js/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import Fec from './app/example-app/components/FecComponent/Fec';
-
-import Home from './pages/Home';
 import SentryBoundary from './utils/SentryBoundary';
 
 const App = () => (
