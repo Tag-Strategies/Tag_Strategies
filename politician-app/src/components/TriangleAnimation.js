31f52dd174a344d5b1cd546c9cde17362cda800c
digiwhale <digiwhale.humphrey@gmail.com>
2020-12-16 16:58:50 -0500

    Removed failed triangle script

---
+++ politician-app/src/components/TriangleAnimation.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import TriangleBG from '../scripts/triangles'
+
+export default class TriangleAnimation extends React.Component{
+
+  componentDidMount () {
+    var canvas = document.getElementById("canvas");
+    var altBG = document.getElementById("altBG");
+    var triangle = new TriangleBG({
+      canvas : canvas,
+      alternateElem : altBG,
+      cellHeight : 120,
+      cellWidth : 100,
+      mouseLight : true,
+      mouseLightRadius : 500,
+      mouseLightIncrement : 10,
+      resizeAdjustment : true,
+      variance : 1.3,
+      pattern : "x*y",
+      baseColor1 : {
+          baseHue : 120,
+          baseSaturation : 60,
+          baseLightness : 38
+      },
+      baseColor2 : {
+          baseHue : 120,
+          baseSaturation : 60,
+          baseLightness : 40
+      },
+      colorDelta : {
+          hue : 1,
+          lightness : 0,
+          saturation : 0
+      }
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <canvas id="canvas"></canvas>
+        <div id="altBG"></div>
+      </div>
+    )
+  }
+}
