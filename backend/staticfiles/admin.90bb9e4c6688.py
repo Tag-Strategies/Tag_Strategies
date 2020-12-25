bddcdf3aab9985ed90228ce9f6a8b22b40ae53d5
digiwhale <digiwhale.humphrey@gmail.com>
2020-12-24 22:38:33 -0500

    Revert "static files for heroku"
    This reverts commit 06af1dcac76b6addd41e554ef472ec65652930e4.

--- backend/staticfiles/admin.90bb9e4c6688.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin  # noqa
-
-from .models import Politician, Organization
-
-# Register your models here.
-
-admin.site.register(Politician)
-admin.site.register(Organization)
--- backend/staticfiles/tables.b1ccf3656b62.py
+++
@@ -1,6 +0,0 @@
-import django_tables2 as tables
-from .models import Politician
-
-class PoliticianTable(tables.Table):
-    class Meta:
-        model = Politician
